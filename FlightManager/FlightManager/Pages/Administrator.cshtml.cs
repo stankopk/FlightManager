ec042b07e142a57c13d7aeb8d783a8c8559a4baa
tnikolaev955 <83333806+tnikolaev955@users.noreply.github.com>
2023-10-27 09:07:23 +0300

    Administrator page with delete user button

---
+++ FlightManager/FlightManager/Pages/Administrator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FlightManager.Pages
+{
+    public class AdministratorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
